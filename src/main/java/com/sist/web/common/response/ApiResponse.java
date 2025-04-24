755a3e7bd112320300af6cdd848643b957d46ebc
hare211 <blackhole2748@gmail.com>
2025-04-24 14:29:09 +0900

    김기현 - 모든 컨트롤러 및 예외 응답 통일, ErrorResponse 제거

--- src/main/java/com/sist/web/common/response/ApiResponse.java
+++ src/main/java/com/sist/web/common/response/ApiResponse.java
@@ -3,21 +3,28 @@ package com.sist.web.common.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse<T> {
-	private boolean success;
+	private String code;
 	private T data;
 	private String message;
 	
-	public static <T> ApiResponse<T> ok(T data) {
-		return new ApiResponse<>(true, data, null);
+	public static <T> ApiResponse<T> success(T data) {
+		return new ApiResponse<>("200", data, "성공");
 	}
 	
-	public static <T> ApiResponse<T> fail(String message) {
-		return new ApiResponse<>(false, null, message);
+	public static <T> ApiResponse<T> success(T data, String message) {
+		return new ApiResponse<>("200", data, message);
+	}
+	
+	public static <T> ApiResponse<T> fail(String code, String message) {
+		return new ApiResponse<>(code, null, message);
+	}
+	
+	public static <T> ApiResponse<T> fail(String code, T data, String message) {
+		return new ApiResponse<>(code, data, message);
 	}
 }
